76fd5f5bce2a8900fe711c459a5129909b6864fc
danielAmar02 <58550965+danielAmar02@users.noreply.github.com>
2021-03-16 16:16:14 +0100

    Create modelvgg16.py

---
+++ framework/modelvgg16.py
@@ -0,0 +1,118 @@
+import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras import layers as kl
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import os
+import cv2 as cv
+import matplotlib.pyplot as plt
+from keras.applications import vgg16
+from keras.models import Model,load_model
+import keras
+from keras.layers import Input, Conv2D, Conv2DTranspose,AveragePooling2D, MaxPooling2D,UpSampling2D,LeakyReLU, concatenate, Dropout,BatchNormalization,Activation
+from keras.callbacks import EarlyStopping,ModelCheckpoint
+import pandas as pd
+from keras.optimizers import Adam
+from sklearn.model_selection import train_test_split
+from numpy.random import seed
+import tensorflow as tf
+
+def ModelEnhancer(input_shape,
+         num_classes=10,
+         output_activation='softmax'):
+  
+    #input_shape = (256, 256, 4)
+    
+    VGG16_weight = "../input/keras-pretrained-models/vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5"
+    VGG16 = vgg16.VGG16(include_top=False, weights=VGG16_weight, input_shape=input_shape)
+    last_layer = VGG16.output
+    
+    set_trainable = False
+    for layer in VGG16.layers:
+        if layer.name in ['block1_conv1']:
+            set_trainable = True
+        if layer.name in ['block1_pool','block2_pool','block3_pool','block4_pool','block5_pool']:
+            layer.trainable = False
+            
+    model_ = Conv2DTranspose(256,(3,3),strides=(2, 2))(last_layer)
+    model_ = LeakyReLU(0.1)(model_)
+    model_ = BatchNormalization()(model_)    
+    
+    concat_1 = concatenate([model_,VGG16.get_layer("block5_conv3").output])
+    
+    model_ = Conv2D(512,(3,3),strides=(1, 1),padding='same')(concat_1)
+    model_ = LeakyReLU(0.1)(model_)
+    model_ = BatchNormalization()(model_)
+    
+    model_ = Conv2DTranspose(512,(3,3),strides=(2, 2),padding='same')(model_)
+    model_ = LeakyReLU(0.1)(model_)
+    model_ = BatchNormalization()(model_) 
+    
+    concat_2 = concatenate([model_,VGG16.get_layer("block4_conv3").output])
+    
+    model_ = Conv2D(512,(3,3),strides=(1, 1),padding='same')(concat_2)
+    model_ = LeakyReLU(0.1)(model_)
+    model_ = BatchNormalization()(model_)
+    
+    model_ = Conv2DTranspose(512,(3,3),strides=(2, 2))(model_)
+    model_ = LeakyReLU(0.1)(model_)
+    model_ = BatchNormalization()(model_) 
+    
+    concat_3 = concatenate([model_,VGG16.get_layer("block3_conv3").output])
+    
+    model_ = Conv2D(256,(3,3),strides=(1, 1),padding='same')(concat_3)
+    model_ = LeakyReLU(0.1)(model_)
+    model_ = BatchNormalization()(model_)
+    
+    model_ = Conv2DTranspose(256,(3,3),strides=(2, 2),padding='same')(model_)
+    model_ = LeakyReLU(0.1)(model_)
+    model_ = BatchNormalization()(model_) 
+    
+    concat_4 = concatenate([model_,VGG16.get_layer("block2_conv2").output])
+    
+    model_ = Conv2D(128,(3,3),strides=(1, 1),padding='same')(concat_4)
+    model_ = LeakyReLU(0.1)(model_)
+    model_ = BatchNormalization()(model_)
+    
+    model_ = Conv2DTranspose(128,(3,3),strides=(2, 2),padding='same')(model_)
+    model_ = LeakyReLU(0.1)(model_)
+    model_ = BatchNormalization()(model_) 
+    
+    concat_5 = concatenate([model_,VGG16.get_layer("block1_conv2").output])
+    
+    model_ = Conv2D(64,(3,3),strides=(1, 1),padding='same')(concat_5)
+    model_ = LeakyReLU(0.1)(model_)
+    model_ = BatchNormalization()(model_)
+    
+    '''model_ = Conv2D(32,(3,3),strides=(1, 1),padding='same')(model_)
+    model_ = LeakyReLU(0.1)(model_)
+    model_ = BatchNormalization()(model_)'''
+    
+    #model_ = Conv2D(3,(3,3),strides=(1, 1),padding='same')(model_)
+    #model_ = LeakyReLU(0.1)(model_)
+    #model_ = BatchNormalization()(model_)
+    
+    model_ = Conv2D(num_classes, kernel_size=(1,1), strides=(1,1), activation=output_activation, padding='valid') (x)
+        
+    
+    model_ = Model(VGG16.input,model_)
+    
+    return model_
+  
+  
+if __name__ == '__main__':
+    # Test
+    vgg_unet = dict(
+        input_shape=(256, 256, 4),
+        num_classes=10,
+        output_activation='softmax')
+    
+    print(f"Creating U-Net with arguments: {vgg_unet}")
+    model = ModelEnhancer(**vgg_unet)
+    print("Summary:")
+    print(model.summary())  
+  
+  
+  
+  
